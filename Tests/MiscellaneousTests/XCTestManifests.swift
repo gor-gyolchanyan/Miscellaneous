66253d2ae0756ec56a4858eafaf5d6e5b807a0e7
Gor Gyolchanyan <gor.zone@icloud.com>
2019-03-14 19:17:46 +0300

    Improved `XCTestManifests.swift`.

--- Tests/MiscellaneousTests/XCTestManifests.swift
+++ Tests/MiscellaneousTests/XCTestManifests.swift
@@ -27,7 +27,7 @@
 
 import XCTest
 
-#if !os(macOS)
+#if !(os(macOS) || os(iOS) || os(tvOS) || os(watchOS))
 public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(MiscellaneousTests.allTests),
